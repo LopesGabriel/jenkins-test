2aece6550e0a78cfda2de34269b328cae1a68919
Gabriel Lopes <lopesgabriel0199@gmail.com>
2021-09-21 21:56:13 -0300

    Adiciona novo controller e sobe versão (Teste production deployment)

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -11,12 +11,17 @@ public class IndexController {
 
 	@GetMapping
 	public String helloWorld() {
-		return "Hello World v1.0.1";
+		return "Hello World v1.1.0";
 	}
 
 	@GetMapping(value = "/{slug}")
 	public String novoEndpoint(@PathVariable(name = "slug") String slug) {
 		return "Você tentou acessar o end-point \"/" + slug + "\".";
+	}
+
+	@GetMapping(value = "/im-alive")
+	public String imAlive() {
+		return "Yeaaah! I am Alive. And running on version 1.1.0";
 	}
 
 }
