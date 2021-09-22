e1a6541bf78b337e09d9491251b5ff1a0e414b9c
Gabriel Lopes <lopesgabriel0199@gmail.com>
2021-09-21 21:38:56 -0300

    Adiciona novo endpoint

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,12 @@ public class IndexController {
 
 	@GetMapping
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World v1.0.1";
+	}
+
+	@GetMapping(value = "/{slug}")
+	public String novoEndpoint(@PathVariable(name = "slug") String slug) {
+		return "Você tentou acessar o end-point \"/" + slug + "\".";
 	}
 
 }
